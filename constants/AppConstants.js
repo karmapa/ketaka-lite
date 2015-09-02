a95cdfee3dc2acdf4b649cfa8e4a84aa9cfabab9
kmsheng <kmsh3ng@gmail.com>
2015-09-03 00:24:21 +0800

    rename events

--- constants/AppConstants.js
+++ constants/AppConstants.js
@@ -9,10 +9,10 @@ export const DIRECTION_VERTICAL = true;
 export const TOGGLE_DIRECTION = 'TOGGLE_DIRECTION';
 export const TOGGLE_READONLY = 'TOGGLE_READONLY';
 
-export const EVENT_ON_SAVE = 'EVENT_ON_SAVE';
-export const EVENT_ON_IMPORT = 'EVENT_ON_IMPORT';
-export const EVENT_ON_OPEN_DOC = 'EVENT_ON_OPEN_DOC';
-export const EVENT_ON_ACTIVATE_TAB = 'EVENT_ON_ACTIVATE_TAB';
+export const EVENT_SAVE = 'EVENT_SAVE';
+export const EVENT_IMPORT = 'EVENT_IMPORT';
+export const EVENT_OPEN_DOC = 'EVENT_OPEN_DOC';
+export const EVENT_ACTIVATE_TAB = 'EVENT_ACTIVATE_TAB';
 
 export const PB_FILENAME_REGEXP = new RegExp('^(.+)_PB\.xml$');
 
