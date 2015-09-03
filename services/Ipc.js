a4ba9b39374a9d85103d081fb161133166d7471c
kmsheng <kmsh3ng@gmail.com>
2015-09-03 23:16:21 +0800

    add file Ipc.js

---
+++ services/Ipc.js
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+import uuid from 'node-uuid';
+
+let ipc = window.require('ipc');
+
+export default class Ipc {
+
+  static events = {};
+
+  static send(name, args = {}) {
+    return new Promise((resolve, reject) => {
+
+      let id = uuid.v4();
+
+      this.initEvent(name);
+      this.events[name].push({id, resolve, reject});
+
+      args.id = id;
+      ipc.send(name, args);
+    });
+  }
+
+  static initEvent(name) {
+    let self = this;
+    if (_.isUndefined(self.events[name])) {
+      self.events[name] = [];
+      ipc.on(name, function(res) {
+
+        let event = _.find(self.events[name], {id: res.id});
+
+        if (! event) {
+          return;
+        }
+        if (res.error) {
+          event.reject(res);
+        }
+        else {
+          event.resolve(res);
+        }
+      });
+    }
+  }
+}
