19381fba0539201e48e1753cd3986b0da14c7f17
kmsheng <kmsh3ng@gmail.com>
2015-09-04 15:57:56 +0800

    remove funciton propsEqual

--- services/Helper.js
+++ services/Helper.js
@@ -3,10 +3,6 @@ import humps from 'humps';
 
 export default class Helper {
 
-  static propsEqual(props, oldObj, newObj) {
-    return props.every(prop => _.isEqual(_.get(oldObj, prop), _.get(newObj, prop)));
-  }
-
   static camelize(fromNames, obj) {
     let toNames = fromNames.map(name => humps.camelize(name));
     fromNames.forEach((name, index) => {
@@ -14,4 +10,5 @@ export default class Helper {
     });
     return obj;
   }
+
 }
