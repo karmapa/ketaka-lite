1b42a0360a497fada3b682be2d7f78141329ab6f
kmsheng <kmsh3ng@gmail.com>
2015-09-12 00:40:14 +0800

    add file build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ROOT_DIR=$(cd $(dirname $0)/..; pwd)
+cd $ROOT_DIR
+
+mkdir -p dist
+webpack
+sed -i '' -e 's/http:\/\/localhost:3000\///g' dist/index.html
+cp -r package.json main.js main dist/
+
+cd dist
+npm install --verbose --production
