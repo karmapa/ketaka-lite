6969fa87e41682e58305e76387f06969abfd8e1e
kmsheng <kmsh3ng@gmail.com>
2015-10-16 03:33:58 +0800

    change image regexp format

--- main/constants.js
+++ main/constants.js
@@ -7,6 +7,6 @@ module.exports = {
   PATH_APP: appPath,
   PATH_APP_DOC: Path.resolve(appPath, 'docs'),
   PATH_APP_CACHE: Path.resolve(app.getPath('userCache'), 'zip-cache'),
-  REGEXP_IMAGE: new RegExp('^([a-zA-Z0-9]+)\\-(\\d+)\\-(\\d+)([abcd])$'),
+  REGEXP_IMAGE: new RegExp('^(\\d+)\\-(\\d+)([abcd])$'),
   REGEXP_PAGE: new RegExp('^(\\d+)\\.(\\d+)([abcd])$')
 };
