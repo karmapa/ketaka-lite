6988bc0b639a08befc7ee5f6fb002f7f5246c5d6
kmsheng <kmsh3ng@gmail.com>
2015-10-27 23:54:39 +0800

    set NODE_ENV

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 cd $ROOT_DIR
 
 mkdir -p dist
-webpack
+NODE_ENV=production webpack
 sed -i '' -e 's/http:\/\/localhost:3000\///g' dist/index.html
 cp -r package.json main.js main dist/
 
