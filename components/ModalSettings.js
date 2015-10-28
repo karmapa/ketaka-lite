805af852450db02f6b301977f8cbeb10f0ab9305
kmsheng <kmsh3ng@gmail.com>
2015-10-29 05:56:47 +0800

    add keyboard shortcuts in component ModalSettings

--- components/ModalSettings.js
+++ components/ModalSettings.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
 import {Button, Modal, Input} from 'react-bootstrap';
+import {ComboListenerInput} from '.';
 
 import {DIRECTION_HORIZONTAL, DIRECTION_VERTICAL} from '../constants/AppConstants';
 
@@ -61,6 +62,69 @@ export default class ModalSettings extends React.Component {
               <Input type='radio' label='Horizontal' onChange={this.onDirectionChange} checked={DIRECTION_HORIZONTAL === direction} value={DIRECTION_HORIZONTAL} />
               <Input type='radio' label='Vertical' onChange={this.onDirectionChange} checked={DIRECTION_VERTICAL === direction} value={DIRECTION_VERTICAL} />
             </div>
+            <label>Keyboard Shortcuts</label>
+            <ul>
+              <li>
+                <span className="combo-text">Add a new tab</span>
+                <ComboListenerInput className="combo-listener" value="cmd + j" />
+              </li>
+              <li>
+                <span className="combo-text">Close current tab</span>
+                <ComboListenerInput className="combo-listener" value="cmd + k" />
+              </li>
+              <li>
+                <span className="combo-text">Switch to the previous tab</span>
+                <ComboListenerInput className="combo-listener" value="ctrl + alt + left" />
+              </li>
+              <li>
+                <span className="combo-text">Switch to the next tab</span>
+                <ComboListenerInput className="combo-listener" value="ctrl + alt + right" />
+              </li>
+              <li>
+                <span className="combo-text">Save the current bamboo</span>
+                <ComboListenerInput className="combo-listener" value="ctrl + s" />
+              </li>
+              <li>
+                <span className="combo-text">Switch input method</span>
+                <ComboListenerInput className="combo-listener" value="alt + space" />
+              </li>
+              <li>
+                <span className="combo-text">Find</span>
+                <ComboListenerInput className="combo-listener" value="ctrl + f" />
+              </li>
+              <li>
+                <span className="combo-text">Find Next</span>
+                <ComboListenerInput className="combo-listener" value="enter" />
+              </li>
+              <li>
+                <span className="combo-text">Find Previous</span>
+                <ComboListenerInput className="combo-listener" value="shift + enter" />
+              </li>
+              <li>
+                <span className="combo-text">Replace</span>
+                <ComboListenerInput className="combo-listener" value="shift + ctrl + f" />
+              </li>
+              <li>
+                <span className="combo-text">Confirm Replace</span>
+                <ComboListenerInput className="combo-listener" value="y" />
+              </li>
+              <li>
+                <span className="combo-text">Confirm Reject</span>
+                <ComboListenerInput className="combo-listener" value="n" />
+              </li>
+              <li>
+                <span className="combo-text">Stop</span>
+                <ComboListenerInput className="combo-listener" value="esc" />
+              </li>
+              <li>
+                <span className="combo-text">Replace All</span>
+                <ComboListenerInput className="combo-listener" value="shift + enter" />
+              </li>
+              <li>
+                <span className="combo-text">Split Page</span>
+                <ComboListenerInput className="combo-listener" value="ctrl + enter" />
+              </li>
+            </ul>
           </div>
         </Modal.Body>
         <Modal.Footer>
