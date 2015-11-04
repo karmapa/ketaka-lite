d76a97f9a775e21e96b52c802caf077ad1a1bb6d
kmsheng <kmsh3ng@gmail.com>
2015-11-05 03:01:55 +0800

    update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 cd $ROOT_DIR
 
 mkdir -p dist
-NODE_ENV=production webpack
+NODE_ENV=production webpack -p
 sed -i '' -e 's/http:\/\/localhost:3000\///g' dist/index.html
 cp -r *.ico *.icns package.json main.js main dist/
 
