cb4f785a39e91a9b7f62e0fd1979f93c9857308e
kmsheng <kmsh3ng@gmail.com>
2016-02-23 19:27:35 +0800

    sort PropTypes

--- components/ImeInput.js
+++ components/ImeInput.js
@@ -7,8 +7,8 @@ import {Ime} from '../services';
 export default class ImeInput extends React.Component {
 
   static PropTypes = {
-    inputMethod: PropTypes.string.isRequired,
     className: PropTypes.string,
+    inputMethod: PropTypes.string.isRequired,
     onChange: PropTypes.func,
     onKeyDown: PropTypes.func,
     onKeyPress: PropTypes.func,
