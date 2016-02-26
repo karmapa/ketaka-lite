62402220ce49689f8e9d6a21b2505f346293b857
kmsheng <kmsh3ng@gmail.com>
2016-02-26 14:57:47 +0800

    export services

--- main/services/index.js
+++ main/services/index.js
@@ -1,7 +1,13 @@
-import MenuConfig from './MenuConfig';
+import Doc from './Doc';
 import Helper from './Helper';
+import Importer from './Importer';
+import MenuConfig from './MenuConfig';
+import getNonContinuousPageNames from './getNonContinuousPageNames';
 
 export {
+  Doc,
+  Helper,
+  Importer,
   MenuConfig,
-  Helper
+  getNonContinuousPageNames
 };
