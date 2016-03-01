b505d923ac7d27b26765a08a4f344b5c2dafbeab
kmsheng <kmsh3ng@gmail.com>
2016-03-01 10:48:08 +0800

    refactor addDoc()

--- main/controllers/addDoc.js
+++ main/controllers/addDoc.js
@@ -3,28 +3,19 @@ import {Doc, Helper} from '../services';
 import {PATH_APP_DOC} from '../constants/appConstants';
 import {ipcHandler} from '../decorators';
 
-let addDoc = ipcHandler(function(event) {
+let addDoc = ipcHandler(async function(event) {
 
   let send = this.send;
-  let docName;
+  let docName = await Doc.findUniqueUntitledName();
 
-  Doc.findUniqueUntitledName()
-    .then(function(name) {
-      docName = name;
-      return Helper.mkdirp(Path.resolve(PATH_APP_DOC, name, 'images'));
-    })
-    .then(function() {
-      let doc = Doc.createDoc({name: docName});
-      let page = Doc.createPage({name: 'untitled'});
-      doc.pages.push(page);
-      return Doc.writeDoc(doc);
-    })
-    .then(function(doc) {
-      send({doc: doc});
-    })
-    .catch(function(err) {
-      send({error: true, message: err});
-    });
+  await Helper.mkdirp(Path.resolve(PATH_APP_DOC, docName, 'images'));
+
+  let doc = Doc.createDoc({name: docName});
+  let page = Doc.createPage({name: 'untitled'});
+  doc.pages.push(page);
+
+  await Doc.writeDoc(doc);
+  send({doc});
 });
 
 export default addDoc;
