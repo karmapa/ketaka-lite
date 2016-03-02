2d4bcd8311ebb02a947366f1262d78b33a740f24
kmsheng <kmsh3ng@gmail.com>
2016-03-02 16:32:27 +0800

    use ipc.once to fix memory leak warning

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,3 @@
-import _ from 'lodash';
-import uuid from 'node-uuid';
-
 let ipc = window.require('ipc');
 
 export default class Api {
@@ -9,37 +6,21 @@ export default class Api {
 
   static send(name, args = {}) {
     return new Promise((resolve, reject) => {
-
-      let id = uuid.v4();
-
-      this.initEvent(name);
-      this.events[name].push({id, resolve, reject});
-
-      args.id = id;
+      this.initEvent(name, resolve, reject);
       ipc.send(name, args);
     });
   }
 
-  static initEvent(name) {
-    let self = this;
-    if (_.isUndefined(self.events[name])) {
-      self.events[name] = [];
-      ipc.on(name, function(res) {
+  static initEvent(name, resolve, reject) {
 
-        let event = _.find(self.events[name], {id: res.id});
-
-        if (! event) {
-          return;
-        }
-        if (res.error) {
-          event.reject(res);
-        }
-        else {
-          event.resolve(res);
-        }
-        delete self.events[name];
-      });
-    }
+    ipc.once(name, function(res) {
+      if (res.error) {
+        reject(res);
+      }
+      else {
+        resolve(res);
+      }
+    });
   }
 
   static on = ipc.on.bind(ipc);
