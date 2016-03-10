7bbeddc749cf991c69949bacaa9c7857ddd300f3
kmsheng <kmsh3ng@gmail.com>
2016-03-10 21:10:56 +0800

    export tag

--- main/services/index.js
+++ main/services/index.js
@@ -3,11 +3,13 @@ import Helper from './Helper';
 import Importer from './Importer';
 import MenuConfig from './MenuConfig';
 import getNonContinuousPageNames from './getNonContinuousPageNames';
+import Tag from './Tag';
 
 export {
   Doc,
   Helper,
   Importer,
   MenuConfig,
+  Tag,
   getNonContinuousPageNames
 };
