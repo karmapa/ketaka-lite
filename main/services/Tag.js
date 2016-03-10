87d8e67712c12f1925e8fef6b950dd0310d6f6b0
kmsheng <kmsh3ng@gmail.com>
2016-03-10 21:14:01 +0800

    refactor tag service

--- main/services/Tag.js
+++ main/services/Tag.js
@@ -4,27 +4,27 @@ export function attrsToStr(attrs) {
   return _.map(attrs, (value, attr) => attr + '="' + value + '"').join(' ');
 }
 
-export function tagToStr(node) {
-
-  if (node.children) {
-    return '<' + node.name + ' ' + attrsToStr(node.attribs) + '>' + node.children.map(tagToStr).join('') + '</' + node.name + '>';
-  }
-  else if ('tag' === node.type) {
-    return '<' + node.name + ' ' + attrsToStr(node.attribs) + '/>';
-  }
-  else if ('text' === node.type) {
-    return node.data;
-  }
-}
-
 export function isTag(node) {
   return 'tag' === node.type;
 }
 
 export function isPbTag(node) {
-  return ('tag' === node.type) && ('pb' === node.name);
+  return isTag(node) && ('pb' === node.name);
 }
 
 export function isTextNode(node) {
   return 'text' === node.type;
+}
+
+export function tagToStr(node) {
+
+  if (node.children) {
+    return '<' + node.name + ' ' + attrsToStr(node.attribs) + '>' + node.children.map(tagToStr).join('') + '</' + node.name + '>';
+  }
+  else if (isTag(node)) {
+    return '<' + node.name + ' ' + attrsToStr(node.attribs) + '/>';
+  }
+  else if (isTextNode(node)) {
+    return node.data;
+  }
 }
