a7de9d65f9a7b0c854d8b536834a8af53ef1ecd8
kmsheng <kmsh3ng@gmail.com>
2016-03-31 15:18:07 +0800

    add module modalEditDocs

--- src/modules/reducer.js
+++ src/modules/reducer.js
@@ -1,10 +1,12 @@
 import app from './app';
 import doc from './doc';
 import modalImport from './modalImport';
+import modalEditDocs from './modalEditDocs';
 import {combineReducers} from 'redux';
 
 export default combineReducers({
   app,
   doc,
-  modalImport
+  modalImport,
+  modalEditDocs
 });
