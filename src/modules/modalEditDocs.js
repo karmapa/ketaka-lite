f88a69af124baed6be83821a7e5218334f422ee6
kmsheng <kmsh3ng@gmail.com>
2016-03-31 20:48:36 +0800

    update module modalEditDocs

--- src/modules/modalEditDocs.js
+++ src/modules/modalEditDocs.js
@@ -1,7 +1,16 @@
+import Api from '../services/Api';
+
 const CLOSE_MODAL = 'ketaka-lite/modalEditDocs/CLOSE_MODAL';
 const OPEN_MODAL = 'ketaka-lite/modalEditDocs/OPEN_MODAL';
+const LOAD_SUCCESS = 'ketaka-lite/modalEditDocs/LOAD_SUCCESS';
+const SET_DOCS = 'ketaka-lite/modalEditDocs/SET_DOCS';
+const SHOW_WARNING = 'ketaka-lite/modalEditDocs/SHOW_WARNING';
+const HIDE_WARNING = 'ketaka-lite/modalEditDocs/HIDE_WARNING';
+
 
 const initialState = {
+  docs: [],
+  isWarningVisible: false,
   isModalVisible: false
 };
 
@@ -9,7 +18,15 @@ const actionsMap = {
 
   [OPEN_MODAL]: (state, action) => Object.assign({}, state, {isModalVisible: action.isModalVisible}),
 
-  [CLOSE_MODAL]: (state, action) => Object.assign({}, state, {isModalVisible: action.isModalVisible})
+  [CLOSE_MODAL]: (state, action) => Object.assign({}, state, {isModalVisible: action.isModalVisible}),
+
+  [LOAD_SUCCESS]: (state, action) => Object.assign({}, state, {docs: action.docs}),
+
+  [SET_DOCS]: (state, action) => Object.assign({}, state, {docs: action.docs}),
+
+  [SHOW_WARNING]: (state, action) => Object.assign({}, state, {isWarningVisible: action.isWarningVisible}),
+
+  [HIDE_WARNING]: (state, action) => Object.assign({}, state, {isWarningVisible: action.isWarningVisible})
 
 };
 
@@ -29,5 +46,37 @@ export function closeModal() {
   return {
     type: CLOSE_MODAL,
     isModalVisible: false
+  };
+}
+
+export function setDocs(docs) {
+  return {
+    type: SET_DOCS,
+    docs
+  };
+}
+
+export function showWarning() {
+  return {
+    type: SHOW_WARNING,
+    isWarningVisible: true
+  };
+}
+
+export function hideWarning() {
+  return {
+    type: HIDE_WARNING,
+    isWarningVisible: false
+  };
+}
+
+export function loadDocNames() {
+
+  return dispatch => {
+    return Api.send('list-doc-name')
+      .then(res => dispatch({
+        type: LOAD_SUCCESS,
+        docs: res.docNames.map(name => ({name, checked: false}))
+      }));
   };
 }
