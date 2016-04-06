29a2f0dc6350c0c6182c498ae36d7a0a1ad9d2c0
kmsheng <kmsh3ng@gmail.com>
2016-04-06 18:44:17 +0800

    fix issue #139

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+ELECTRON_VERSION=0.36.9
+FOLDER_ID=0B0sfHY8vpXYhSGVLUmpuQ0JKbEE
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs)
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
-cd $ROOT_DIR
+ZIP_IOS="KETAKA-Lite-darwin-x64-v${PACKAGE_VERSION}.zip"
+ZIP_WIN="KETAKA-Lite-win32-ia32-v${PACKAGE_VERSION}.zip"
 
+cd $ROOT_DIR
 rm -rf dist
 mkdir -p dist
 NODE_ENV=production webpack
@@ -13,14 +17,10 @@ cp -r assets/images/*.ico assets/images/*.icns .babelrc package.json index.js ma
 cd dist
 npm install --verbose --production
 
-ZIP_WIN="KETAKA-Lite-win32-ia32-v${PACKAGE_VERSION}.zip"
-ZIP_IOS="KETAKA-Lite-darwin-x64-v${PACKAGE_VERSION}.zip"
-FOLDER_ID=0B0sfHY8vpXYhSGVLUmpuQ0JKbEE
-
-electron-packager ./ KETAKA-Lite --platform=win32 --arch=ia32 --version=0.36.9 --icon=treasure_logo.ico &&
+electron-packager ./ KETAKA-Lite --platform=win32 --arch=ia32 --version="${ELECTRON_VERSION}" --app-version="${PACKAGE_VERSION}" --icon=treasure_logo.ico &&
 zip -r "${ZIP_WIN}" KETAKA-Lite-win32-ia32
 gdrive upload "${ZIP_WIN}" -p "${FOLDER_ID}" &&
 rm -r KETAKA-Lite-win32-ia32* &&
-electron-packager ./ KETAKA-Lite --platform=darwin --arch=x64 --version=0.36.9 --icon=treasure_logo.icns &&
+electron-packager ./ KETAKA-Lite --platform=darwin --arch=x64 --version="${ELECTRON_VERSION}" --app-version="${PACKAGE_VERSION}" --icon=treasure_logo.icns &&
 zip -r "${ZIP_IOS}" KETAKA-Lite-darwin-x64 &&
 gdrive upload "${ZIP_IOS}" -p "${FOLDER_ID}"
