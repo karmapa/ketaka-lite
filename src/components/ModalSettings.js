283ac4a0e10eef2f0875b2968154f1cf256279d1
kmsheng <kmsh3ng@gmail.com>
2016-04-07 23:17:38 +0800

    re-style

--- src/components/ModalSettings.js
+++ src/components/ModalSettings.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
 import {Button, Modal, Input} from 'react-bootstrap';
 import {ComboListenerInput} from '.';
