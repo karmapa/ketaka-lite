dfc9316c58f789ed7d187328ab24f22117c3ab76
kmsheng <kmsh3ng@gmail.com>
2016-04-14 23:13:35 +0800

    re-style

--- main/constants/appConstants.js
+++ main/constants/appConstants.js
@@ -1,12 +1,9 @@
 import Path from 'path';
 import {app} from 'electron';
 
-const appPath = Path.resolve(app.getPath('appData'), 'ketaka-lite');
-
-export const PATH_APP = appPath;
-export const PATH_APP_DOC = Path.resolve(appPath, 'docs');
+export const PATH_APP = Path.resolve(app.getPath('appData'), 'ketaka-lite');
+export const PATH_APP_DOC = Path.resolve(PATH_APP, 'docs');
 export const PATH_APP_CACHE = Path.resolve(app.getPath('userCache'), 'zip-cache');
 export const REGEXP_IMAGE = new RegExp('^(\\d+[abcde]?)\\-(\\d+)([abcd])$');
 export const REGEXP_PAGE = new RegExp('^(\\d+[abcde]?)\\.(\\d+)([abcd])$');
-
 export const VALID_IMAGE_EXTENSIONS = ['bmp', 'gif', 'jpg', 'png'];
