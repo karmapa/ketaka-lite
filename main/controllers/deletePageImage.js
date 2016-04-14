f422e570afbc2a0ebbc9b97c47d00fbeadcbb807
kmsheng <kmsh3ng@gmail.com>
2016-04-14 23:09:32 +0800

    remove console.log

--- main/controllers/deletePageImage.js
+++ main/controllers/deletePageImage.js
@@ -7,8 +7,6 @@ const PATH_APP_DOC = constants.PATH_APP_DOC;
 
 const deletePageImage = ipcHandler(async function(event, res) {
 
-  console.log('???', res);
-
   const send = this.send;
   const path = Path.resolve(PATH_APP_DOC, res.docName, 'images', res.imageFilename);
 
@@ -16,9 +14,6 @@ const deletePageImage = ipcHandler(async function(event, res) {
     send();
     return;
   }
-
-  console.log('here', path);
-
   await Helper.rimraf(path)
   send();
 });
