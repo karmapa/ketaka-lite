be393523ca4a82eb33f89c105b1730d8f56b68d2
kmsheng <kmsh3ng@gmail.com>
2016-04-14 23:11:37 +0800

    re-style

--- main/services/index.js
+++ main/services/index.js
@@ -1,17 +1,7 @@
-import Doc from './Doc';
-import Helper from './Helper';
-import Importer from './Importer';
-import MenuConfig from './MenuConfig';
-import getNonContinuousPageNames from './getNonContinuousPageNames';
-import Tag from './Tag';
-import Message from './Message';
-
-export {
-  Doc,
-  Helper,
-  Importer,
-  MenuConfig,
-  Message,
-  Tag,
-  getNonContinuousPageNames
-};
+export Doc from './Doc';
+export Helper from './Helper';
+export Importer from './Importer';
+export MenuConfig from './MenuConfig';
+export getNonContinuousPageNames from './getNonContinuousPageNames';
+export Tag from './Tag';
+export Message from './Message';
