68f9010716f286d89eb8a2f35025ee6bf32d024e
kmsheng <kmsh3ng@gmail.com>
2016-11-15 12:03:06 +0800

    use const instead of let

--- main/services/__tests__/getNonContinuousPageNames-test.js
+++ main/services/__tests__/getNonContinuousPageNames-test.js
@@ -6,32 +6,32 @@ const getNonContinuousPageNames = require('../getNonContinuousPageNames').defaul
 describe('getNonContinuousPageNames', () => {
 
   it('getNonContinuousPageNames case 1: ', () => {
-    let input = ['1.1a', '1.2a', '1.2b', '1.3a', '1.3b'];
-    let output = ['1.1b'];
+    const input = ['1.1a', '1.2a', '1.2b', '1.3a', '1.3b'];
+    const output = ['1.1b'];
     expect(getNonContinuousPageNames(input)).toEqual(output);
   });
 
   it('getNonContinuousPageNames case 2: ', () => {
-    let input = ['1.1a', '1.1b', '1.2a', '1.2b', '1.2c', '1.3a', '1.3b'];
-    let output = ['1.2d'];
+    const input = ['1.1a', '1.1b', '1.2a', '1.2b', '1.2c', '1.3a', '1.3b'];
+    const output = ['1.2d'];
     expect(getNonContinuousPageNames(input)).toEqual(output);
   });
 
   it('getNonContinuousPageNames case 3: ', () => {
-    let input = ['1.1a', '1.1b', '1.3a', '1.3b'];
-    let output = [];
+    const input = ['1.1a', '1.1b', '1.3a', '1.3b'];
+    const output = [];
     expect(getNonContinuousPageNames(input)).toEqual(output);
   });
 
   it('getNonContinuousPageNames case 4: ', () => {
-    let input = ['1.1a', '1.1b', '2.1a', '2.1b'];
-    let output = [];
+    const input = ['1.1a', '1.1b', '2.1a', '2.1b'];
+    const output = [];
     expect(getNonContinuousPageNames(input)).toEqual(output);
   });
 
   it('getNonContinuousPageNames case 5: ', () => {
-    let input = ['1.1a', '1.1b', '1.1c', '1.1d', '1.2a', '1.2b', '1.3a', '1.3b', '1.3c', '1.3d'];
-    let output = [];
+    const input = ['1.1a', '1.1b', '1.1c', '1.1d', '1.2a', '1.2b', '1.3a', '1.3b', '1.3c', '1.3d'];
+    const output = [];
     expect(getNonContinuousPageNames(input)).toEqual(output);
   });
 
