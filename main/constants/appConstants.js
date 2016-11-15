74a77fe59c7c4b01f52bfd89634427c064a1112c
kmsheng <kmsh3ng@gmail.com>
2016-11-16 01:18:55 +0800

    put REGEXP_IMAGE to separated file

--- main/constants/appConstants.js
+++ main/constants/appConstants.js
@@ -4,5 +4,4 @@ import {app} from 'electron';
 export const PATH_APP = Path.resolve(app.getPath('appData'), 'ketaka-lite');
 export const PATH_APP_DOC = Path.resolve(PATH_APP, 'docs');
 export const PATH_APP_CACHE = Path.resolve(app.getPath('userCache'), 'zip-cache');
-export const REGEXP_IMAGE = new RegExp('^(\\d+[abcde]?)\\-(\\d+)([abcd])$');
 export const VALID_IMAGE_EXTENSIONS = ['bmp', 'gif', 'jpg', 'png'];
---
+++ main/constants/regexpImage.js
@@ -0,0 +1,3 @@
+const REGEXP_IMAGE = new RegExp('^(\\d+[abcde]?)\\-(\\d+)([abcd])$');
+
+export default REGEXP_IMAGE;
