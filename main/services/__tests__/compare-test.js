09100a3781836d92db0f28916d8f08aecc717914
kmsheng <kmsh3ng@gmail.com>
2016-11-25 23:11:44 +0800

    update compare-test.js

--- main/services/__tests__/compare-test.js
+++ main/services/__tests__/compare-test.js
@@ -1,4 +1,5 @@
 jest.unmock('./../compare');
+jest.unmock('./../parsePbId');
 jest.unmock('./../../constants/regexpPage');
 const compare = require('../compare').default;
 
@@ -11,5 +12,41 @@ describe('compare', () => {
     expect(result).toEqual(output);
   });
 
+  it('github issue #180', () => {
+    const input = [
+      '1-1-1a',
+      '1-1-1b',
+      '1-1-10a',
+      '1-1-10b',
+      '1-1-2a',
+      '1-1-2b',
+      '1-10-1a',
+      '1-10-1b',
+      '1-2-1a',
+      '1-2-1b',
+      '10-1-1a',
+      '10-1-1b',
+      '2-1-1a',
+      '2-1-1b'
+    ];
+    const output = [
+      '1-1-1a',
+      '1-1-1b',
+      '1-1-2a',
+      '1-1-2b',
+      '1-1-10a',
+      '1-1-10b',
+      '1-2-1a',
+      '1-2-1b',
+      '1-10-1a',
+      '1-10-1b',
+      '2-1-1a',
+      '2-1-1b',
+      '10-1-1a',
+      '10-1-1b'
+    ];
+    const result = input.sort(compare);
+    expect(result).toEqual(output);
+  });
 });
 
