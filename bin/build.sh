adb6baaaed396fb441ab5471e8afdca00405bd17
kmsheng <kmsh3ng@gmail.com>
2017-09-05 16:33:41 +0800

    refactor build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 ELECTRON_VERSION=0.36.9
-FOLDER_ID=0B0sfHY8vpXYhSGVLUmpuQ0JKbEE
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs)
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
-ZIP_IOS="KETAKA-Lite-darwin-x64-v${PACKAGE_VERSION}.zip"
-ZIP_WIN="KETAKA-Lite-win32-ia32-v${PACKAGE_VERSION}.zip"
 
 cd $ROOT_DIR
 rm -rf dist
@@ -15,12 +12,16 @@ sed -i '' -e 's/http:\/\/localhost:3000\///g' dist/index.html
 cp -r assets/images/*.ico assets/images/*.icns .babelrc package.json index.js main.js main dist/
 
 cd dist
-npm install --verbose --production &&
+npm install --production
 
-electron-packager ./ KETAKA-Lite --platform=win32 --arch=ia32 --version="${ELECTRON_VERSION}" --app-version="${PACKAGE_VERSION}" --icon=treasure_logo.ico &&
-zip -r "${ZIP_WIN}" KETAKA-Lite-win32-ia32
-gdrive upload "${ZIP_WIN}" -p "${FOLDER_ID}" &&
-rm -rf KETAKA-Lite-win32-ia32* &&
-electron-packager ./ KETAKA-Lite --platform=darwin --arch=x64 --version="${ELECTRON_VERSION}" --app-version="${PACKAGE_VERSION}" --icon=treasure_logo.icns &&
-zip -r "${ZIP_IOS}" KETAKA-Lite-darwin-x64 &&
-gdrive upload "${ZIP_IOS}" -p "${FOLDER_ID}"
+electron-packager ./ KETAKA-Lite --platform=win32 --arch=ia32 --version="${ELECTRON_VERSION}" --app-version="${PACKAGE_VERSION}" --icon=treasure_logo.ico
+zip -r "KETAKA-Lite-win32-ia32-v${PACKAGE_VERSION}.zip" KETAKA-Lite-win32-ia32
+rm -rf KETAKA-Lite-win32-ia32*
+
+electron-packager ./ KETAKA-Lite --platform=win32 --arch=x64 --version="${ELECTRON_VERSION}" --app-version="${PACKAGE_VERSION}" --icon=treasure_logo.ico
+zip -r "KETAKA-Lite-win32-x64-v${PACKAGE_VERSION}.zip" KETAKA-Lite-win32-ia32
+rm -rf KETAKA-Lite-win32-x64*
+
+electron-packager ./ KETAKA-Lite --platform=darwin --arch=x64 --version="${ELECTRON_VERSION}" --app-version="${PACKAGE_VERSION}" --icon=treasure_logo.icns
+zip -r "KETAKA-Lite-darwin-x64-v${PACKAGE_VERSION}.zip" KETAKA-Lite-darwin-x64
+rm -rf KETAKA-Lite-darwin-x64
